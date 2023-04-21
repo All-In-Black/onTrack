9a2cda0d5025b421971ce01e8561e7128895cf06
andrei_solovev_2 <sir.noen@yandex.ru>
2023-04-21 17:36:19 +0300

    create vue composition api app

---
+++ ontrack/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <TheHeader />
+
+  <main class="flex flex-grow flex-col">
+
+  </main>
+
+  <TheNav />
+
+</template>
+
+<script setup>
+  import TheHeader from "@/components/TheHeader.vue";
+  import TheNav from "@/components/TheNav.vue";
+
+</script>
---
+++ ontrack/src/components/TheLogo.vue
@@ -0,0 +1,13 @@
+<template>
+  <a href="#" >
+    <img src="../assets/logo.png" alt="Logo" class="h-9">
+  </a>
+</template>
+
+<script>
+
+</script>
+
+<style scoped>
+
+</style>
