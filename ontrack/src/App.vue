983057be1ad108219169c77648e4786364784d6a
andrei_solovev_2 <sir.noen@yandex.ru>
2023-04-24 16:01:06 +0300

    update..to be continued

--- ontrack/src/App.vue
+++ ontrack/src/App.vue
@@ -2,14 +2,34 @@
   <TheHeader />
 
   <main class="flex flex-grow flex-col">
-
+    <TheTimeline v-show="currentPage === PAGE_TIMELINE" />
+    <TheActivities v-show="currentPage === PAGE_ACTIVITIES" />
+    <TheProgress v-show="currentPage === PAGE_PROGRESS" />
   </main>
 
-  <TheNav />
+  <TheNav :current-page="currentPage" @navigate="currentPage = $event" />
 
 </template>
 
 <script setup>
   import TheHeader from "@/components/TheHeader.vue";
   import TheNav from "@/components/TheNav.vue";
+  import TheTimeline from "@/pages/TheTimeline.vue";
+  import TheActivities from "@/pages/TheActivities.vue";
+  import TheProgress from "@/pages/TheProgress.vue";
+  import {PAGE_ACTIVITIES, PAGE_PROGRESS, PAGE_TIMELINE} from "@/constants";
+  import {ref} from "vue";
+
+  const currentPage = ref(normalizeHashPage())
+  function normalizeHashPage() {
+    const hash = window.location.hash.slice(1)
+
+    if ([PAGE_ACTIVITIES, PAGE_PROGRESS, PAGE_TIMELINE].includes(hash)) {
+      return hash
+    }
+    window.location.hash = PAGE_TIMELINE
+
+    return PAGE_TIMELINE
+  }
+
 </script>
--- ontrack/src/components/TheNav.vue
+++ ontrack/src/components/TheNav.vue
@@ -2,7 +2,14 @@
   <nav class="sticky bottom-0 z-10 bg-white">
     <ul class="flex items-center justify-around border-t">
 
-      <NavItem v-for="(icon, page) in navItems" :key="page" :href="`#${page}`">
+      <NavItem
+          v-for="(icon, page) in navItems"
+          :key="page"
+          :href="`#${page}`"
+          :class="{ 'bg-gray-200 pointer-events-none': page === currentPage }"
+          @click = "emit('navigate', page)"
+      >
+
         <component :is="icon" class="h-6 w-6" /> {{ page }}
       </NavItem>
 
@@ -11,14 +18,21 @@
 </template>
 
 <script setup>
-  import NavItem from './NavItem.vue'
-  import { ClockIcon, ListBulletIcon, ChartBarIcon } from '@heroicons/vue/24/outline'
+import {PAGE_ACTIVITIES, PAGE_PROGRESS, PAGE_TIMELINE} from "@/constants";
+import NavItem from './NavItem.vue'
+import { ClockIcon, ListBulletIcon, ChartBarIcon } from '@heroicons/vue/24/outline'
 
-  const navItems = {
-  timeline: ClockIcon,
-  activities: ListBulletIcon,
-  progress: ChartBarIcon
+defineProps(['currentPage'])
+
+const emit = defineEmits(['navigate'])
+
+const navItems = {
+  [PAGE_TIMELINE]: ClockIcon,
+  [PAGE_ACTIVITIES]: ListBulletIcon,
+  [PAGE_PROGRESS]: ChartBarIcon
 }
+
+
 </script>
 
 
